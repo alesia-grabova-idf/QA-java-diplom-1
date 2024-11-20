6ad50d298430c0aa61cb684965910da34997e977
alesia-grabova-idf <alesia-grabova-idf>
2024-11-20 22:48:01 +0100

    tests fot Ingredient class

---
+++ src/test/java/praktikum/IngredientTest.java
@@ -0,0 +1,27 @@
+package praktikum;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class IngredientTest {
+
+  @Test
+  public void getTypeTest() {
+    Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "Test Sauce", 50.0f);
+    assertEquals(IngredientType.SAUCE, ingredient.getType());
+  }
+
+  @Test
+  public void getNameTest() {
+    Ingredient ingredient = new Ingredient(IngredientType.FILLING, "Test Filling", 75.0f);
+    assertEquals("Test Filling", ingredient.getName());
+  }
+
+  @Test
+  public void getPriceTest() {
+    Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "Test Sauce", 50.0f);
+    assertEquals(50.0f, ingredient.getPrice(), 0.01);
+  }
+
+}
