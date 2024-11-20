d6f8a04c25680480ecd3e28537140e2107ed3055
alesia-grabova-idf <alesia-grabova-idf>
2024-11-20 21:41:04 +0100

    add  JUnit4, Mockito, Jacoco tests fot Bun class

---
+++ src/test/java/praktikum/BunTest.java
@@ -0,0 +1,21 @@
+package praktikum;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class BunTest {
+
+  @Test
+  public void getNameTest() {
+    Bun bun = new Bun("Test Bun", 150.0f);
+    assertEquals("Test Bun", bun.getName());
+  }
+
+  @Test
+  public void getPriceTest() {
+    Bun bun = new Bun("Test Bun", 150.0f);
+    assertEquals(150.0f, bun.getPrice(), 0.01);
+  }
+
+}
