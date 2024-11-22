6fafbd58ccbc647217f28227f56c519724f4eff8
alesia-grabova-idf <alesia-grabova-idf>
2024-11-23 00:39:44 +0100

    parametrized tests

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -1,20 +1,46 @@
 package praktikum;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
-
+@RunWith(Parameterized.class)
 public class BunTest {
+
+  private final String name;
+  private final float price;
+
+  // Единственный публичный конструктор
+  public BunTest(String name, float price) {
+    this.name = name;
+    this.price = price;
+  }
+
+  // Метод для предоставления параметров
+  @Parameterized.Parameters
+  public static Collection<Object[]> data() {
+    return Arrays.asList(new Object[][]{
+        {"Test Bun", 150.0f},
+        {"Cheese Bun", 200.0f},
+        {"Whole Grain Bun", 180.5f}
+    });
+  }
 
   @Test
   public void getNameTest() {
-    Bun bun = new Bun("Test Bun", 150.0f);
-    assertEquals("Test Bun", bun.getName());
+    Bun bun = new Bun(name, price);
+    assertEquals(name, bun.getName());
   }
 
   @Test
   public void getPriceTest() {
-    Bun bun = new Bun("Test Bun", 150.0f);
-    assertEquals(150.0f, bun.getPrice(), 0.01);
+    Bun bun = new Bun(name, price);
+    assertEquals(price, bun.getPrice(), 0.01);
   }
 }
+
--- src/test/java/praktikum/IngredientTest.java
+++ src/test/java/praktikum/IngredientTest.java
@@ -2,25 +2,49 @@ package praktikum;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.Collection;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+@RunWith(Parameterized.class)
 public class IngredientTest {
+
+  private final IngredientType type;
+  private final  String name;
+  private final float price;
+
+  public IngredientTest(IngredientType type, String name, float price) {
+    this.type = type;
+    this.name = name;
+    this.price = price;
+  }
+
+  @Parameterized.Parameters
+  public static Collection<Object[]> data() {
+    return Arrays.asList(new Object[][] {
+        { IngredientType.SAUCE, "Test Sauce", 50.0f },
+        { IngredientType.FILLING, "Test Filling", 75.0f },
+        { IngredientType.SAUCE, "Spicy Sauce", 60.0f }
+    });
+  }
 
   @Test
   public void getTypeTest() {
-    Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "Test Sauce", 50.0f);
-    assertEquals(IngredientType.SAUCE, ingredient.getType());
+    Ingredient ingredient = new Ingredient(type, name, price);
+    assertEquals(type, ingredient.getType());
   }
 
   @Test
   public void getNameTest() {
-    Ingredient ingredient = new Ingredient(IngredientType.FILLING, "Test Filling", 75.0f);
-    assertEquals("Test Filling", ingredient.getName());
+    Ingredient ingredient = new Ingredient(type, name, price);
+    assertEquals(name, ingredient.getName());
   }
 
   @Test
   public void getPriceTest() {
-    Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "Test Sauce", 50.0f);
-    assertEquals(50.0f, ingredient.getPrice(), 0.01);
+    Ingredient ingredient = new Ingredient(type, name, price);
+    assertEquals(price, ingredient.getPrice(), 0.01);
   }
 }
